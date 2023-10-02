b5c8f7ac804bbe30ea25edb65da6fd7d1ad71988
AnshulGoyal589 <anshulgoyal589@gmail.com>
2023-10-02 08:37:20 +0530

    Dark mode and Light Mode added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
@@ -6,14 +6,26 @@ import EditPage from './pages/Edit';
 import './App.css'; // Import the CSS file for additional styles
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
   return (
-    <div className="container">
+    <div className={`container ${isDarkMode ? 'dark-mode' : ''}`}>
       <nav className="navbar">
         <div className="navbar-brand">MEME GENERATOR</div>
         <div className="navbar-links">
-          <Link to="/" className="nav-link">
+          <Link to="/" className="nav-link home">
             Home
           </Link>
+          <button
+            onClick={toggleDarkMode}
+            className={`nav-link toggle-button ${isDarkMode ? 'dark' : 'light'}`}
+          >
+            {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+          </button>
         </div>
       </nav>
       <div className="content">
