9ddec2189f829dc1552bc9ee55ada0f2c673ed5b
Udit Padhan <uditpadhan98@gmail.com>
2023-10-12 18:24:22 +0530

    placeholder from input box removed

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -3,14 +3,14 @@ import Draggable from "react-draggable";
 const Text = () => {
   const [editMode, setEditMode] = useState(false);
   const [val, setVal] = useState("Double Click to Edit");
-
+  const [text,setText] = useState("");
   return (
     <Draggable>
       {editMode ? (
         <input
           onDoubleClick={(e) => setEditMode(false)}
-          value={val}
-          onChange={(e) => setVal(e.target.value)}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
       ) : (
         <h2 onDoubleClick={(e) => setEditMode(true)}>{val}</h2>
